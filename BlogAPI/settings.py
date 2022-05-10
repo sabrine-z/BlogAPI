35c02712721284fad90fc18769a7439788fa12c9
sabrine <zouaghisabrine772@gmail.com>
2022-05-10 14:51:57 +0100

    Third commit

--- BlogAPI/settings.py
+++ BlogAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ekv_q(h4!3&patl6^%prtr4w-$jeu$1z65y(69k))m#yk_lenv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-rest-api1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
